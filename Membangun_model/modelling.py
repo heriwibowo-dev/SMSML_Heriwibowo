1dcffa96c23d9ad4f2c73c419b2811377d3286c1
Heri Wibowo <147681086+heriwibowo-dev@users.noreply.github.com>
2026-06-05 03:08:33 +0700

    Integrate DagsHub and update code comments
    Added DagsHub integration for model tracking and updated comments for clarity.

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -5,25 +5,28 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import os
+import dagshub # Tambahkan ini agar terhubung ke DagsHub
 
-# 1. Load Data
-# Kita tentukan path file dengan menunjuk ke direktori utama (root)
-# __file__ adalah lokasi file skrip ini
+# 1. Inisialisasi Koneksi DagsHub
+# Ini akan otomatis mengambil kredensial dari environment variable (Secrets)
+dagshub.init(repo_owner='heriwibowo-dev', repo_name='SMSML_HeriWibowo', mlflow=True)
+
+# 2. Load Data
 script_dir = os.path.dirname(os.path.abspath(__file__))
+# Jika script ada di folder Membangun_model, naik 1 level ke root
 root_dir = os.path.dirname(script_dir)
 data_path = os.path.join(root_dir, 'heart.csv')
 
-# Membaca data menggunakan path yang sudah dikoreksi
 df = pd.read_csv(data_path)
 
-# 2. Preprocessing
+# 3. Preprocessing
 X = df.drop(columns=['target'])
 y = df['target']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-# 3. Training & Tracking dengan MLFlow
+# 4. Training & Tracking dengan MLFlow
 mlflow.set_experiment("Heart_Disease_Prediction")
 
 with mlflow.start_run():
@@ -33,7 +36,7 @@ with mlflow.start_run():
     # Log model ke MLFlow
     mlflow.sklearn.log_model(model, "model")
     
-    # Log metrik (contoh akurasi)
+    # Log metrik
     accuracy = model.score(X_test, y_test)
     mlflow.log_metric("accuracy", accuracy)
     
